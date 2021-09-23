225f72e6bad6a1dbacde28ceca62188d55562649
Lev Leibov <Lev_Leibov@epam.com>
2021-09-24 01:23:07 +0300

    add new methods to make an analysis of results

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 ###### FFt test
 1. In *SplitRadix.py* comparison of built in numpy *fft* and split radix *fft* is presented.
 2. In *SplitRadixWithError.py* the dependence of NMSE of split radix *fft* with error on the number of layer 
-is computed for different standard deviations of Gaussian distributions of error. According to 
-presented graph, the 1st level acts like the most sensitive to noised multiplications. 
-However the definition of levels is important in this analysis
-![Figure_1](result_graph.png)
+is computed for different standard deviations of Gaussian distributions of error. 
+Note: now all the analysis is presented in the google docks file accessible by https://docs.google.com/document/d/1ruvrYftgAMNSGtRNMF21pq_GBoASd_-ipYt4lPVgefc/edit 
--- SplitRadixWithError.py
+++ SplitRadixWithError.py
@@ -12,6 +12,7 @@ from SplitRadix import splitRadixFFT
   {N = 64: layer = 1, N = 32: layer = 2, N = 16: layer = 3, N = 8: layer = 4, N = 4: layer = 5}
   
 * Run the code to generate resulting graph NMSE(layer), which shows the layer sensitivity.
+  Note: now all the analysis is presented in the google docks file accessible by https://docs.google.com/document/d/1ruvrYftgAMNSGtRNMF21pq_GBoASd_-ipYt4lPVgefc/edit 
 '''
 
 # Using Gaussian distribution to add error to multiplication of input and twiddle factor
@@ -38,6 +39,14 @@ def createLayers(N):
     power = int(np.log2(N))
     for layer in range(1, power):
         layerMap[N] = layer
+        N //= 2
+
+def initmultipliesMap(N):
+    global multipliesMap
+    multipliesMap = {}
+    power = int(np.log2(N))
+    for layer in range(1, power):
+        multipliesMap[N] = 0
         N //= 2
 
 
@@ -76,6 +85,7 @@ def splitRadixWithError(signal, N, errorLayer, stdCoef):
         if errorLayer == layer:
             ZkW1 = multiplyWithError(W1, Zk, stdCoef)
             ZdkW3 = multiplyWithError(W3, Zdk, stdCoef)
+            multipliesMap[N] += 2
         else:
             ZkW1 = W1 * Zk
             ZdkW3 = W3 * Zdk
@@ -100,16 +110,17 @@ def calculateError(signal, errorSignal):
 
 # Generate input signal
 def generateInputSignal():
-    t = range(32)
+    t = range(64)
     signal = []
     for i in range(len(t)):
         signal.append(np.exp(-1j * t[i] ** 2))
+        #signal.append(np.exp(-1j * t[i] ** 6))
     return signal
 
 
 def makeErrorAveraging(signal, n, errLayer, stdCoef):
     errors = []
-    numRepeats = 50
+    numRepeats = 20 # nomber of realizations. Should be above 1000 to eliminate noise
     for i in range(numRepeats):
         originSplitRadix = splitRadixFFT(signal, n)
         errorSplitRadix = splitRadixWithError(signal, n, errLayer, stdCoef)
@@ -140,7 +151,49 @@ signalAvg = np.abs(np.average(signal))
 # Generate ffts
 createLayers(n)
 stdCoef = 1 * signalAvg
-errorLayer = 2
+errorLayer = 5
+
+# Count amount of erroneous  multiplies for each layer
+def countMultipliesPerLayer(N, stdCoef):
+    initmultipliesMap(N)
+    signal = generateInputSignal()
+    n = len(signal)
+    createLayers(n)
+    for errLayer in range (1, 6):
+        splitRadixWithError(signal, n, errLayer, stdCoef)
+    layers = list(layerMap.values())
+    multiplies = list(multipliesMap.values())
+    plt.plot(layers, multiplies, '-o', linewidth=0.5)
+    plt.xticks([1, 2, 3, 4, 5])
+    plt.title("Dependence of number of multiplies on layer")
+    plt.xlabel("Layer")
+    plt.ylabel("Number of multiplications on each layer")
+    plt.show()
+
+
+countMultipliesPerLayer(n, stdCoef)
+
+# Visualize dependency of NMSE on stdCoef for each layer
+def errorForDifferentStd(stdCoefs):
+    errs = []
+    errs2 = []
+    for errLayer in range(1, 6):
+        errs = []
+        for std in stdCoefs:
+            err = makeErrorAveraging(signal, n, errLayer, std)
+            errs.append(err)
+        errs2.append(errs)
+    for errArray in errs2:
+        plt.plot(stdCoefs, errArray, linewidth=2)
+        plt.xlabel("Std", fontsize=20)
+        plt.ylabel("NMSE", fontsize=20)
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+    plt.legend(['layer 1', 'layer 2', 'layer 3', 'layer 4', 'layer 5'])
+    plt.show()
+
+
+
 originSplitRadix = splitRadixFFT(signal, n)
 errorSplitRadix = splitRadixWithError(signal, n, errorLayer, stdCoef)
 
@@ -154,18 +207,29 @@ print("Signal avg value = ", signalAvg, "\n")
 print("RESULTS: The 1st level acts like the most sensitive to noised multiplications. However the definition of levels is important in this analysis")
 
 # Compare fft results for different values of standard deviation (sigma) used in Gaussian distribution of error
-stdCoefs = [0.001 * signalAvg, 0.5 * signalAvg, 1 * signalAvg, 5 * signalAvg, 10 * signalAvg]
+#stdCoefs = [0.001 * signalAvg, 0.5 * signalAvg, 1 * signalAvg, 5 * signalAvg, 10 * signalAvg]
+#stdCoefs = [0.001 * signalAvg, 1 * signalAvg]
+stdCoefs = [0.001 * signalAvg, 0.5 * signalAvg, 1 * signalAvg, 2 * signalAvg,
+            3 * signalAvg, 4 * signalAvg, 5 * signalAvg, 6 * signalAvg, 7 * signalAvg, 8 * signalAvg,
+            9 * signalAvg, 10 * signalAvg, 11 * signalAvg, 12 * signalAvg,
+            13 * signalAvg, 14 * signalAvg, 15 * signalAvg]
+            # 16 * signalAvg, 17 * signalAvg, 18 * signalAvg, 19 * signalAvg, 20 * signalAvg,
+            # 21 * signalAvg, 22 * signalAvg, 23 * signalAvg, 24 * signalAvg, 25 * signalAvg]
 for c in stdCoefs:
     result = compareWithDiffLayers(c)
     layers = result[0]
     nmse_vals = result[1]
-    plt.plot(layers, nmse_vals, '-o', linestyle='dashed', linewidth=0.5)
-    plt.legend(['sigma = 0.001*signalAvg', 'sigma = 0.5*signalAvg', 'sigma = 1*signalAvg', 'sigma = 5*signalAvg', 'sigma = 10*signalAvg'])
-    plt.xticks([1, 2, 3, 4, 5])
-    plt.title("Dependence NMSE on layer")
-    plt.xlabel("Layer")
-    plt.ylabel("NMSE")
+    plt.plot(layers, nmse_vals, '-o', linewidth=2)
+    #plt.legend(['sigma = 0.001*signalAvg', 'sigma = 1*signalAvg'])
+    plt.legend(['sigma = 0.001*signalAvg', 'sigma = 0.5*signalAvg', 'sigma = 1*signalAvg', 'sigma = 2*signalAvg', 'sigma = 3*signalAvg', 'sigma = 4*signalAvg',
+                'sigma = 5*signalAvg', 'sigma = 6*signalAvg', 'sigma = 7*signalAvg', 'sigma = 8*signalAvg', 'sigma = 9*signalAvg',
+                'sigma = 10*signalAvg', 'sigma = 11*signalAvg', 'sigma = 12*signalAvg',
+                'sigma = 13*signalAvg', 'sigma = 14*signalAvg', 'sigma = 15*signalAvg'])
+    plt.xticks([1, 2, 3, 4, 5], fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.title("Dependence NMSE on layer", fontsize=20)
+    plt.xlabel("Layer", fontsize=20)
+    plt.ylabel("NMSE", fontsize=20)
 plt.show()
 
-# RESULTS:
-# The 1st level acts like the most sensitive to noised multiplications. However the definition of levels is important in this analysis
+errorForDifferentStd(stdCoefs)
